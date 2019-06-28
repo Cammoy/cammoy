7c88d15574417afeed459195c5fc3462559a165b
Kwasi Brown <kwasibrown1@gmail.com>
2019-06-28 12:26:24 +0100

    fixed footer to bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ import vimeo from '../img/social/vimeo.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
+      <footer className="footer has-background-black has-text-white-ter is-fixed-bottom">
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
