b22fb2de995404d76f8a6a23501178e742a4bd40
Kwasi Brown <kwasibrown1@gmail.com>
2019-07-04 16:06:41 +0100

    update header

--- src/components/Layers/Layers.js
+++ src/components/Layers/Layers.js
@@ -2,32 +2,38 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import "./styles.scss"
 
-const Layer = props => (
-    <div className="col-sm-5">
-        <div className="headerPoints">
-            <div className="headerPoints__layers">
-                Solution Design <span className="bounce-it"> <i className="fa fa-angle-double-down" aria-hidden="true"></i></span>
-                <div className="headerPoints__layers__overlay">
-                Nascetur a suspendisse aenean commodo tortor congue himenaeos augue hac dis morbi per volutpat.
+const Layer = ({ title, subheading }) => (
+    <>
+        <div className="column is-two-fifths">
+            <div className="headerPoints">
+                <div className="headerPoints__layers">
+                    Solution Design <span className="bounce-it"> <i className="fa fa-angle-double-down" aria-hidden="true"></i></span>
+                    <div className="headerPoints__layers__overlay">
+                    Nascetur a suspendisse aenean commodo tortor congue himenaeos augue hac dis morbi per volutpat.
+                    </div>
                 </div>
-            </div>
 
-            <div className="headerPoints__layers">
-                <i className="fa fa-angle-double-down" aria-hidden="true"></i>
-                Implementation <span className="bounce-it"> </span>
-                <div className="headerPoints__layers__overlay">
-                Nascetur a suspendisse aenean commodo tortor congue himenaeos augue hac dis morbi per volutpat.
+                <div className="headerPoints__layers">
+                    <i className="fa fa-angle-double-down" aria-hidden="true"></i>
+                    Implementation <span className="bounce-it"> </span>
+                    <div className="headerPoints__layers__overlay">
+                    Nascetur a suspendisse aenean commodo tortor congue himenaeos augue hac dis morbi per volutpat.
+                    </div>
                 </div>
-            </div>
-            
-            <div className="headerPoints__layers">
-                Optimisation <span className="bounce-it"> <i className="fa fa-angle-double-down" aria-hidden="true"></i></span>
-                <div className="headerPoints__layers__overlay">
-                Nascetur a suspendisse aenean commodo tortor congue himenaeos augue hac dis morbi per volutpat.
+                
+                <div className="headerPoints__layers">
+                    Optimisation <span className="bounce-it"> <i className="fa fa-angle-double-down" aria-hidden="true"></i></span>
+                    <div className="headerPoints__layers__overlay">
+                    Nascetur a suspendisse aenean commodo tortor congue himenaeos augue hac dis morbi per volutpat.
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        <div className="column is-three-fifths">
+            <h1>{title}</h1>
+            <h2>{subheading}</h2>
+        </div>
+    </>
 );
 
 export default Layer;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -26,50 +26,7 @@ export const IndexPageTemplate = ({
         backgroundAttachment: `fixed`,
       }}
     >
-      
-      <div class="columns">
-        <div className="column is-three-fifths">
-          <Layers />
-        </div>
-        <div className="column is-two-fifths">
-          <div
-          style={{
-            display: 'flex',
-            height: '150px',
-            lineHeight: '1',
-            justifyContent: 'space-around',
-            alignItems: 'left',
-            flexDirection: 'column',
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-            style={{
-              boxShadow:
-                'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-              backgroundColor: 'rgb(255, 68, 0)',
-              color: 'white',
-              lineHeight: '1',
-              padding: '0.25em',
-            }}
-          >
-            {title}
-          </h1>
-          <h3
-            className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-            style={{
-              boxShadow:
-                'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-              backgroundColor: 'rgb(255, 68, 0)',
-              color: 'white',
-              lineHeight: '1',
-              padding: '0.25em',
-            }}
-          >
-            {subheading}
-          </h3>
-        </div>
-      </div>
+        <Layers title={title} subheading={subheading} />
       </div>
     </div>
     <section className="section section--gradient">
